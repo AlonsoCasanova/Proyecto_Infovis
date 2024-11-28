0bf723b2423f882e29b077f21595c47d330c70ef
AlonsoCasanova <101608246+AlonsoCasanova@users.noreply.github.com>
2024-11-28 10:42:28 -0300

    Update preprocesamientoE3.py

--- preprocesamientoE3.py
+++ preprocesamientoE3.py
@@ -23,7 +23,27 @@ def consumo_por_seg(tpo_uso, gasto_por_uso):
     
     return consumo_segundo
 
+def consumo_por_min(tpo_uso, gasto_por_uso):   
+    if "hora" in tpo_uso:
+        tpo_uso = extract_numeric(tpo_uso)
+        tiempo_uso_promedio_min = tpo_uso * 60
+    elif "minuto" in tpo_uso:
+        tpo_uso = extract_numeric(tpo_uso)
+        tiempo_uso_promedio_min = tpo_uso
+    elif "continuo" in tpo_uso:
+        tiempo_uso_promedio_min = 1 * 30 * 24 * 60   # No hay datos en más de un mes
+
+    consumo_min = gasto_por_uso / tiempo_uso_promedio_min
+    
+    return consumo_min
+
 datos['Consumo_por_segundo'] = datos.apply(
     lambda row: consumo_por_seg(row['Tiempo_Uso_unitario'], row['Gasto_por_uso(pesos)']),
     axis=1
 )
+
+datos['Consumo_por_minuto'] = datos.apply(
+    lambda row: consumo_por_min(row['Tiempo_Uso_unitario'], row['Gasto_por_uso(pesos)']),
+    axis=1
+)
+
