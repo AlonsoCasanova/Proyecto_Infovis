d924e300e1defece5036ea8d9f9b90d7da235685
AlonsoCasanova <101608246+AlonsoCasanova@users.noreply.github.com>
2024-11-28 10:43:09 -0300

    Create sound.js

---
+++ sound.js
@@ -0,0 +1,22 @@
+import NotePlayer from './js/notePlayer.js';
+import Protobject from './js/protobject.js'; // Importa el objeto Protobject para la comunicación
+
+const options = {
+    C2: "https://tonejs.github.io/audio/casio/C2.mp3",
+    G2: "https://tonejs.github.io/audio/casio/G2.mp3"
+    // Add more notes as needed
+};
+const player = new NotePlayer(options);
+
+player.onInstrumentLoaded(() => {
+    console.log("Instrument loaded successfully.");
+});
+
+Protobject.onReceived((data) => {
+	if (data.play=="1") {
+		player.play("C2"); // Play note A1
+    }
+  	if (data.play=="2") {
+    	player.play("G2")	
+    }
+});
