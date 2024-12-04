7696e3c19514577850b07370c7e32ac733e10458
AlonsoCasanova <101608246+AlonsoCasanova@users.noreply.github.com>
2024-12-03 21:23:23 -0300

    Update graphE3.js

--- graphE3.js
+++ graphE3.js
@@ -8,18 +8,19 @@ var layout = {
     height: 500,  
     width: 750,   
     xaxis: {
-        title: 'Tiempo'
+        title: 'Tiempo',
+      	showgrid: false
     },
     yaxis: {
         title: 'Gasto acumulado(pesos)',
       	tick0: 0,  
-      	range: [0, 10],  // Fixed scale: the y-axis will go from 0 to 60
-        zeroline: true,  // Show the zero line
-        showgrid: true,  // Show gridlines
+      	range: [0, 1],  
+        zeroline: true,  
+        showgrid: false,
     },
   	line: {
         color: 'blue',      
-        dash: 'dot',       
+        dash: '',       
         width: 4   
   	},
 };
@@ -29,31 +30,59 @@ let startTime = Date.now();
 var xData = [0];
 var yData = [0];
 var time;
+var diff = false;
+var prev = 0;
 
 // Escucha los datos recibidos desde Protobject
 Protobject.onReceived((data) => {
   time = (Date.now() - startTime) / 1000;
   xData.push(time);
-  console.log(time)
   if (data.show=="1") {
-	yData.push(yData.at(-1) + 0.5)
+    yData.push(yData.at(-1) + 0.015)
+    if (prev != 1){
+    	diff = true;
+      	prev = 1
+    }
   } else if (data.show=="2") {
-	yData.push(yData.at(-1) + 1)    
-  } 
-  else {
+	yData.push(yData.at(-1) + 0.005);
+    if (prev != 2){
+    	diff = true;
+      	prev = 2;
+    }
+  } else {
     yData.push(yData.at(-1))
+    prev = 0;
   }
   
   if (xData.length > 12){
   	xData = xData.slice(-12);	
     yData = yData.slice(-12);
   }
+  
+  if (diff == true){
+      Plotly.relayout('myDiv', {
+      shapes: [
+          ...layout.shapes || [],
+          {
+              type: 'line',
+              x0: xData.at(-2),
+              y0: 0.1,
+              x1: xData.at(-2),
+              y1: 10,
+              line: { color: 'rgb(37, 150, 190)', width: 1 }
+          }
+      ]
+  	});
+    diff = false;
+  }
+  
   var datos = [
       {
         x: xData,
         y: yData,
-        type: 'scatter'
+        type: 'lines'
       }
     ];
+  
   Plotly.newPlot('myDiv', datos, layout);
 });
