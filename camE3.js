ad2bdedbd5c087b31ee9056ee4ff1b0498ce7021
Tomás Aguirrebeña <127432854+TomasAguirrebena@users.noreply.github.com>
2024-12-04 17:22:12 -0300

    Update camE3.js

--- camE3.js
+++ camE3.js
@@ -1,28 +1,22 @@
 import PresenceSensor from './js/presenceSensor.js';
 import Protobject from './js/protobject.js';
 
-PresenceSensor.setBaselineAndStartMonitoring(5400, 0); //5400 is the frequency of detection; 0 is the webcam id
+PresenceSensor.setBaselineAndStartMonitoring(2000, 1); //5400 is the frequency of detection; 0 is the webcam id
 											// 5400=5 seg aprox 
-PresenceSensor.showPreview({ top: 50, left: 50, width: 640, height: 480 });
+PresenceSensor.showPreview({ top: 0, left: 0, width: 640, height: 480 });
 
 let prev = 0;
 
 PresenceSensor.onData((similarity) => {
   console.log("Image similarity:" + similarity);
-  if (similarity<0.75)  {
-  	Protobject.send({ show: '2' }).to('graph.js');
-    if (prev != 2){
-    	Protobject.send({ play: '2' }).to('sound.js');
-      	prev = 2;
-    }
-  } else if (similarity<0.93) {
+
+  if (similarity < 0.65) {
     Protobject.send({ show: '1' }).to('graph.js');
-    if (prev != 1){
-    	Protobject.send({ play: '1' }).to('sound.js');
-      prev = 1;
-    }
+    Protobject.send({ play: '1' }).to('sound.js');
+  } else if (similarity < 0.9) {
+    Protobject.send({ show: '2' }).to('graph.js');
+    Protobject.send({ play: '2' }).to('sound.js');
   } else {
     Protobject.send({ show: 'off' }).to('graph.js');
   }
-
 });
